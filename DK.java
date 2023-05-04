cbfc17e9672c61535723827dd80918af70db06ad
Dhanush Kumar S <115974135+Dhanushkumardk@users.noreply.github.com>
2023-05-04 16:59:18 +0530

    Add files via upload

---
+++ DK.java
@@ -0,0 +1,59 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DK {
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	ChromeOptions co = new ChromeOptions();
+    	co.addArguments("--remote-allow-origins=*");
+    	WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(co);
+        driver.get("https://amzn.eu/d/1WEvPqK");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        String etitle = "(Renewed) Redmi Note 7 Pro (Space Black, 128GB, 6GB RAM)";
+        System.out.println(title);
+        if(title.equals(etitle)) {
+        	System.out.println("Title is matching");
+        }
+        else {
+        	System.out.println("Title Mismatch");
+        }
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+        Thread.sleep(5000); 
+        Boolean present = driver.findElement(By.xpath("//*[@id=\"attach-close_sideSheet-link\"]")).isDisplayed();
+        System.out.println("Is item added to cart:" + present);
+        driver.get("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@value=\"Delete\"]")).click();
+        Thread.sleep(2000);
+        String removeText = driver.findElement(By.xpath("//*[@id=\"sc-active-cart\"]/div/div[1]/div/h1")).getText();
+        String extext = "Your Amazon Cart is empty.";
+        if(removeText.equals(extext)) {
+        	System.out.println("Product is removed");
+        }
+        driver.get("https://www.amazon.in/dp/B08D9KQTSV?ref_=cm_sw_r_cp_ud_dp_3CX295TEZRK8MCBMGESJ");
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+        Thread.sleep(2000);
+        driver.get("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"a-autoid-0-announce\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"quantity_2\"]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@name=\"proceedToRetailCheckout\"] "));
+        Thread.sleep(1000);
+//        String sign = driver.getTitle();
+//        System.out.println(sign);
+//        String esign = "Amazon Sign In";
+//        if(sign.equals(esign)) {
+//        	System.out.println("Success");
+//        }
+//		/*
+//		 * else { System.out.println("Failure"); }
+//		 */
+    }
+}
